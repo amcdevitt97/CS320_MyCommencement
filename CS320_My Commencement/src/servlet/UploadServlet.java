5d3a268abd2994c56fd7948956b6047450060662
Robert Politi <rpoliti@ycp.edu>
2018-04-25 13:38:54 -0400

    UploadServlet progress

---
+++ CS320_My Commencement/src/servlet/UploadServlet.java
@@ -0,0 +1,88 @@
+package servlet;
+ 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+ 
+@WebServlet("/uploadServlet")
+@MultipartConfig(maxFileSize = 16177215)    // upload file's size up to 16MB
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    private String url = "http://localhost:8081/amcdevitt/login";
+    private String user = "rpoliti";
+    private String pass = "password";
+    
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+         
+        InputStream inputStream = null;
+         
+        // obtains the upload file part in this multipart request
+        Part filePart = request.getPart("photo");
+        if (filePart != null) {
+            // testing
+            System.out.println(filePart.getName());
+            System.out.println(filePart.getSize());
+            System.out.println(filePart.getContentType());
+             
+            // obtains input stream of the upload file
+            inputStream = filePart.getInputStream();
+        }
+         
+        Connection conn = null;
+        String message = null;
+         
+        try {
+            // connects to the database
+            //DriverManager.registerDriver(Class.forName(className));
+            conn = DriverManager.getConnection(url, user, pass);
+ 
+            // constructs SQL statement
+            String sql = "INSERT INTO contacts (first_name, last_name, photo) values (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            
+            if (inputStream != null) {
+                statement.setBlob(3, inputStream);
+            }
+ 
+            // sends the statement to the database server
+            int row = statement.executeUpdate();
+            if (row > 0) {
+                message = "File uploaded and saved into database";
+            }
+        } catch (SQLException ex) {
+            message = "ERROR: " + ex.getMessage();
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                // closes the database connection
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            // sets the message in request scope
+            request.setAttribute("Message", message);
+             
+            // forwards to the message page
+            getServletContext().getRequestDispatcher("/Message.jsp").forward(request, response);
+        }
+    }
+}
